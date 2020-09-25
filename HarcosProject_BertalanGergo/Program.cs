abb0ef082847ff72094d4933990ef287a34be4c4
BertalanGergo <gerib2001@gmail.com>
2020-09-25 10:04:44 +0200

    Feladat kezdete.Project létrehozasa.

---
+++ HarcosProject_BertalanGergo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HarcosProject_BertalanGergo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
