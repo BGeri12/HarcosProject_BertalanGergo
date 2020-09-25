9d5fa6e71e78bebf266de0952b4b426ca865a18a
BertalanGergo <gerib2001@gmail.com>
2020-09-25 11:17:14 +0200

    Adat tagok létrehozása.Konstruktor létrehozása.Getter setter létehozása..

--- HarcosProject_BertalanGergo/Harcos.cs
+++ HarcosProject_BertalanGergo/Harcos.cs
@@ -13,8 +13,36 @@ namespace HarcosProject_BertalanGergo
         private int alapEletero;
         private int alapSebzes;
 
-        
+        public Harcos(string nev, int szint, int tapasztalat, int eletero, int alapEletero, int alapSebzes, int statuszSablon)
+        {
 
+            this.nev = nev;
+            szint = 1;
+            tapasztalat = 0;
+
+            if (alapEletero == 15)
+            {
+                alapSebzes = 3;
+            }else if (alapEletero == 12)
+            {
+                alapSebzes = 4;
+            }
+            else if(alapEletero == 8)
+            {
+                alapSebzes = 5;
+            }
+
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public int Szint { get => szint; set => szint = value; }
+        public int Tapasztalat { get => tapasztalat; set => tapasztalat = value; }
+        public int AlapEletero { get => alapEletero; }
+        public int AlapSebzes { get => alapSebzes; }
+        public int Eletero { get => eletero; set => eletero = value; }
+        public int Sebzes { get => alapSebzes + szint; }
+        public int SzintLepeshez { get => 10 + szint * 5; }
+        public int MaxEletero { get => AlapEletero + szint * 3; }
 
 
 
