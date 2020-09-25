e5a02331c28913e8b191f100b01e45cad68dae46
BertalanGergo <gerib2001@gmail.com>
2020-09-25 10:37:38 +0200

    Adat  tagok felvétele.

--- HarcosProject_BertalanGergo/Harcos.cs
+++ HarcosProject_BertalanGergo/Harcos.cs
@@ -6,5 +6,17 @@ namespace HarcosProject_BertalanGergo
 {
     class Harcos
     {
+        private string nev;
+        private int szint;
+        private int tapasztalat;
+        private int eletero;
+        private int alapEletero;
+        private int alapSebzes;
+
+        
+
+
+
+
     }
 }
