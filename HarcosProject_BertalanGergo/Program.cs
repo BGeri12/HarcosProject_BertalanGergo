b7b47901b4706b832cd481a856cda13f4b1caf46
BGeri12 <gerib2001@gmail.com>
2020-10-03 15:57:12 +0200

    Statikus feltöltés

--- HarcosProject_BertalanGergo/Program.cs
+++ HarcosProject_BertalanGergo/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace HarcosProject_BertalanGergo
 {
     class Program
     {
+        static List<Harcos> lista = new List<Harcos>();
         static void Main(string[] args)
         {
-            
+            StatikusFeltoltes();
+
+
+            Console.ReadKey();
+        }
+
+        private static void StatikusFeltoltes()
+        {
+            Harcos harcos1 = new Harcos("Jancsi", 2);
+            Harcos harcos2 = new Harcos("Pisti", 3);
+            Harcos harcos3 = new Harcos("Karoly", 1);
+            lista.Add(harcos1);
+            lista.Add(harcos2);
+            lista.Add(harcos3);
         }
     }
 }
